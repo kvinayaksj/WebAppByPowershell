3db9e28a62d67df4ec49d254eed51fe6db26c2a8
Vinayak <kvinayak_s@yahoo.com>
2018-05-20 18:00:35 +0530

    Add project files.

---
+++ WebCICDDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebCICDDemo.Startup))]
+namespace WebCICDDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
